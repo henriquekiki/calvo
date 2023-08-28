bced0ad945897576f49c89b5b32dc408774d6907
henriquekiki <ricckdmo@gmail.com>
2023-08-28 18:45:31 -0300

    Bug do tempo fixo resolvido

--- calvo.py
+++ calvo.py
@@ -8,20 +8,21 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents)
 
 timers = {
-    "minato": {"fixed_time": datetime.timedelta(minutes=120), "custom_time": None, "reset": False},
-    "mini minato": {"fixed_time": datetime.timedelta(minutes=60), "custom_time": None, "reset": False},
-    "d reaper": {"fixed_time": datetime.timedelta(minutes=60), "custom_time": None, "reset": False},
+    "minato": {"fixed_time": datetime.timedelta(minutes=120), "custom_time": None},
+    "mini minato": {"fixed_time": datetime.timedelta(minutes=60), "custom_time": None},
+    "d reaper": {"fixed_time": datetime.timedelta(minutes=60), "custom_time": None},
 }
 
 def format_time(td):
     hours, remainder = divmod(td.seconds, 3600)
     minutes, _ = divmod(remainder, 60)
-    return f"{hours:02}H{minutes:02}M"
+    return f"{hours:02}H {minutes:02}M"
 
 @bot.command()
 async def minato(ctx, custom_minutes: int = None):
     if custom_minutes is not None:
         timers['minato']['custom_time'] = datetime.timedelta(minutes=custom_minutes)
+        timers['minato']['fixed_time'] = datetime.timedelta(minutes=120)
         await ctx.send(f"Run de Minato iniciará em {custom_minutes} minutos e depois seguirá o tempo fixo.")
     else:
         await ctx.send(f"Tempo aproximado para Run de Minato: {format_time(timers['minato']['custom_time'] or timers['minato']['fixed_time'])} CH 0")
@@ -30,6 +31,7 @@ async def minato(ctx, custom_minutes: int = None):
 async def miniminato(ctx, custom_minutes: int = None):
     if custom_minutes is not None:
         timers['mini minato']['custom_time'] = datetime.timedelta(minutes=custom_minutes)
+        timers['mini minato']['fixed_time'] = datetime.timedelta(minutes=60)
         await ctx.send(f"Mini run Minato iniciará em {custom_minutes} minutos e depois seguirá o tempo fixo.")
     else:
         await ctx.send(f"Tempo aproximado para Mini run Minato: {format_time(timers['mini minato']['custom_time'] or timers['mini minato']['fixed_time'])} CH 0")
@@ -38,6 +40,7 @@ async def miniminato(ctx, custom_minutes: int = None):
 async def dreaper(ctx, custom_minutes: int = None):
     if custom_minutes is not None:
         timers['d reaper']['custom_time'] = datetime.timedelta(minutes=custom_minutes)
+        timers['d reaper']['fixed_time'] = datetime.timedelta(minutes=60)
         await ctx.send(f"D-Reaper iniciará em {custom_minutes} minutos e depois seguirá o tempo fixo.")
     else:
         await ctx.send(f"Tempo aproximado para D-Reaper: {format_time(timers['d reaper']['custom_time'] or timers['d reaper']['fixed_time'])} CH 0")
@@ -49,15 +52,10 @@ async def update_timers():
             timer['custom_time'] -= datetime.timedelta(seconds=1)
         elif timer['fixed_time'].total_seconds() > 0:
             timer['fixed_time'] -= datetime.timedelta(seconds=1)
-        else:
-            timer['fixed_time'] = timers[timer_name]['original_fixed_time']
 
 @bot.event
 async def on_ready():
     print(f'Bot conectado como {bot.user.name}')
-    for timer in timers.values():
-        timer['original_custom_time'] = timer['custom_time']
-        timer['original_fixed_time'] = timer['fixed_time']
     update_timers.start()
 
 discord_token = 'MTE0NTc5ODIzMTY1NDE0MTk3Mg.Gjk0Yo.G_OQb8_XMASZjsAH280zt9tZRHK8_0ZwCJds1E'
